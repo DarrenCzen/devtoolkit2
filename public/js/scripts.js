40c26daec3908bde6ce0bf23732a3d064dce5963
Darren <zen_darren@hotmail.com>
2024-03-07 19:43:17 +0800

    added jquery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,42 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function submitSubscription(event) {
+  // var usernameValue = $("#username").val(); // Get value of username input
+  // var emailValue = $("#email").val(); // Get value of email input
+  // console.log("Username: " + usernameValue + ", Email: " + emailValue);
+
+  //   let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io";
+
+  //   var settings = {
+  //     url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
+  //     method: "GET",
+  //     timeout: 0,
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   };
+
+  //   var myArray = [];
+  //   console.log("calling API");
+
+  //   $.ajax(settings).done(function (response) {
+  //     myArray = response;
+  //     buildTable(myArray);
+  //     console.log(myArray);
+  //   });
+  event.preventDefault();
+}
+
+$(document).ready(function () {
+  $("#subscriptionForm").submit(function (e) {
+    e.preventDefault(); // Prevent form submission
+    var serializedData = $(this).serialize(); // Serialize form data
+
+    var formData = {};
+
+    serializedData.split("&").forEach(function (item) {
+      var keyValue = item.split("=");
+      formData[keyValue[0]] = decodeURIComponent(keyValue[1] || "");
+    });
+
+    console.log(formData);
+  });
+});
