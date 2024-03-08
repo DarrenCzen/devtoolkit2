e8352e46e9b96fe2da573146a0092f4b18bc44cb
Darren <zen_darren@hotmail.com>
2024-03-08 10:51:59 +0800

    refactor to test endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const user = require("../backend/api/users");
+const test = require("../backend/api/test");
 
 let app = express();
 
 app.use(express.json());
+
 app.use("/users", user.router);
-
-router = express.Router();
-
-router.get("/", (request, response) => {
-  response.send("Hello World");
-});
-
-app.use(router);
+app.use("/", test.router);
 
 app.listen(3000, (errors) => {
   if (errors) console.error("not working");
